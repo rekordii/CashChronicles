c31cccdb332ec98277bfe7f323c0b9d05ecbebc0
Marius <mariuswenk20@gmail.com>
2025-10-03 14:39:11 +0200

    add windows comparability

--- pages/month_overview.py
+++ pages/month_overview.py
@@ -201,7 +201,7 @@ def display_month_overview(parent, pages, month_to_display, window):
 
     def on_back():
         W_WIDTH = int(window.winfo_screenwidth() * 0.3)
-        W_HEIGHT = int(window.winfo_screenheight() * 0.6)
+        W_HEIGHT = int(window.winfo_screenheight() * 0.75)
         WINDOW_SIZE = f"{W_WIDTH}x{W_HEIGHT}"
         plt.close("all")
         pages["month_menu"].tkraise()
@@ -245,7 +245,7 @@ def get_summary(month_name):
     return income, expense, balance
 
 def sort_col(tree, col, reverse):
-    data = [(tree.set(k, col), k) for k in tree.get_children("")] 
+    data = [(tree.set(k, col), k) for k in tree.get_children("")]
     try:
         data.sort(key=lambda t: float(t[0]), reverse=reverse)
     except ValueError:
